0989cd32d91169d12baf8d930bfb9b39f095d1aa
LuTanDat <ludathoc@gmail.com>
2024-06-16 09:55:48 +0700

    update video 2,3

---
+++ video 3/script.js
@@ -0,0 +1,6 @@
+let a = 1;
+let b = 2;
+
+console.log(`Hello '' "" from js`) // dung dau backtik de show dau nhay don, nhay kep thoai mai
+console.log(`The sum of a and b is ${a + b}`); // tinh toan trong chuoi
+console.log(`The sum of a and b is`, a + b);
