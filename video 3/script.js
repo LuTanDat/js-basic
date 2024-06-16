cc2ce1a3eea14f9b22810a910b22ee974c59bd27
LuTanDat <ludathoc@gmail.com>
2024-06-16 10:28:59 +0700

    update_chapter2_video 2,3

--- video 3/script.js
+++ video 3/script.js
@@ -1,6 +1,12 @@
-let a = 1;
+let a = 1; // variables
 let b = 2;
+
+const c = 5; // constant, khong dc gan lai
 
 console.log(`Hello '' "" from js`) // dung dau backtik de show dau nhay don, nhay kep thoai mai
 console.log(`The sum of a and b is ${a + b}`); // tinh toan trong chuoi
 console.log(`The sum of a and b is`, a + b);
+
+
+let x = 5, y = 6, z = 7;
+console.log("Gia tri 3 bien la: ", x, y, z); // in ra 3 bien x, y, z
