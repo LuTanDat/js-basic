c8b1c19b9900a36bb1a31372e2bb61e134bf54db
LuTanDat <ludathoc@gmail.com>
2024-06-24 11:27:33 +0700

    array

---
+++ video 9/script.js
@@ -0,0 +1,19 @@
+console.log("Hello world");
+
+// Array [item1, item2, item3,...]
+const arr = ['Ann', 'john', 'jee']
+
+// dung nhieu nhat, luu vao db
+const family = [
+  {
+    name: "John",
+    age: 30,
+  },
+  {
+    name: "Jee",
+    age: 20,
+  },
+]
+
+console.log(arr[0]); // Ann
+console.log(family[1].age);// 20
