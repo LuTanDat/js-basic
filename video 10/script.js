00168db318937c00047f4b973945b29f866e8442
LuTanDat <ludathoc@gmail.com>
2024-06-24 21:53:33 +0700

    c2-v8-compare

---
+++ video 10/script.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+
+// compare
+// == ss gia tri, nghia la no tu dong convert sang so
+// === ss gia tri và kieu du lieu
+
+let a = 5;
+let b = '5';
+
+console.log(a == b);// true
+console.log(a === b);// false -> Recommend
+
+//------------------------------------------------------------------------------------------
+
+
+// empty, null, undefined
+
+//empty
+let c = '' // or let c = []
+
+// null -> khong chiem dia chi bo nho
+let d = null;
+console.log(d)
+
+// undefined: chua khai bao bien or khai bao bien nhung khong co gia tri
+let e;
+console.log(e);
+
+// null vs empty dung nao cung dc
