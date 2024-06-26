7b7e0c64abc873b04fa870e085577281bc0d7b43
LuTanDat <ludathoc@gmail.com>
2024-06-26 10:05:18 +0700

    c3-v1-for loop

---
+++ video 11/script.js
@@ -0,0 +1,27 @@
+console.log("Hello world");
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+let arr = ['mu', 'mancity', 'liver', 'chelsea']
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+}
+
+// trong vong lap i++ or ++i deu giong nhau
+// truong hop binh thuong
+// i++: assign -> increment
+// ++i: increment -> assign
+
+i = 1;
+j = ++i;
+//(i is 2, j is 2)
+console.log(i, j);
+
+i = 1;
+j = i++;
+//(i is 2, j is 1)
+console.log(i, j);
+
+
